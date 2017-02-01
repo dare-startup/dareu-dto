3720da69a12719ad5c577a78365473c567359e65
Alberto <arubalcaba@24hourfit.com>
2017-01-31 16:12:29 -0800

    Updated AbstractMessage

--- src/main/java/com/dareu/web/dto/response/message/AbstractMessage.java
+++ src/main/java/com/dareu/web/dto/response/message/AbstractMessage.java
@@ -5,6 +5,8 @@ package com.dareu.web.dto.response.message;
  * @author Alberto Rubalcaba <arubalcaba@24hourfit.com>
  */
 public abstract class AbstractMessage {
+    
+    public static final String MESSAGE_TYPE = "messageType";
     private String messageType;
     
     public AbstractMessage(MessageType messageType){
