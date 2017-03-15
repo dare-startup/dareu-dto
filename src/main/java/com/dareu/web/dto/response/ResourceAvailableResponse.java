83d1b4ea7285344944fa2335c7948f124b13a2cc
Alberto <arubalcaba@24hourfit.com>
2017-03-15 12:18:14 -0700

    Updated dto

--- src/main/java/com/dareu/web/dto/response/ResourceAvailableResponse.java
+++ src/main/java/com/dareu/web/dto/response/ResourceAvailableResponse.java
@@ -1,7 +1,7 @@
 package com.dareu.web.dto.response;
 
 public class ResourceAvailableResponse {
-	private boolean isAvailable; 
+	private boolean available;
 	private String message; 
 	private String date;
 	public ResourceAvailableResponse() {
@@ -11,18 +11,19 @@ public class ResourceAvailableResponse {
 	
 	public ResourceAvailableResponse(boolean isAvailable, String message,
 			String date) {
-		this.isAvailable = isAvailable;
+		this.available = isAvailable;
 		this.message = message;
 		this.date = date;
 	}
 
-
 	public boolean isAvailable() {
-		return isAvailable;
+		return available;
 	}
-	public void setAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
+
+	public void setAvailable(boolean available) {
+		this.available = available;
 	}
+
 	public String getMessage() {
 		return message;
 	}
